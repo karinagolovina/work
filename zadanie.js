b32cd42b8e880943c2a403be51ed6ece018b5676
karinagolovina <120774604+karinagolovina@users.noreply.github.com>
2023-09-13 14:27:28 +0300

    Create zadanie.js

---
+++ zadanie.js
@@ -0,0 +1,23 @@
+function index(){
+let n = prompt("Enput n: ");
+let text = prompt("Enput text: ");
+const makeItFunny = () => {
+	let result = '';
+	let i=0;
+	while (i < text.length) {
+		const current= text[i];
+		if((i+1) % n === 0){
+			result = `${result}${current.toUpperCase()}`;
+		}else{
+			result= `${result}${current}`;
+		}
+		i++;
+	}
+	alert(result);
+	
+}
+
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", index);
